29d32efcc9fc4283988e76f66f22d458de629263
Sowchirukuri <lakshmisowmya.chirukuri@gmail.com>
2023-09-25 09:56:37 -0500

    define columns

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,9 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category'); 
+const Tag = require('./Tag'); 
+const ProductTag = require('./ProductTag');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,8 +12,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
-  },
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          isDecimal: true,
+        },
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 10, // Set a default value of 10
+        validate: {
+          isNumeric: true,
+        },
+      },
+      category_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category', // This should match the modelName defined for the Category model
+          key: 'id',
+        },
+      },
+    },
   {
     sequelize,
     timestamps: false,
@@ -19,5 +53,13 @@ Product.init(
     modelName: 'product',
   }
 );
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 module.exports = Product;
