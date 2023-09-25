f4603484afd19ffaf7f9f6b407cdce871d768c48
Sowchirukuri <lakshmisowmya.chirukuri@gmail.com>
2023-09-25 10:05:18 -0500

    define columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,32 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
+const Product = require('./Product'); 
+const Tag = require('./Tag'); 
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', 
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', 
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -16,5 +36,14 @@ ProductTag.init(
     modelName: 'product_tag',
   }
 );
+
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+});
 
 module.exports = ProductTag;
